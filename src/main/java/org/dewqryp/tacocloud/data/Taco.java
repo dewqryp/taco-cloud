c2620a7f560d816fa04cb6473dc002b82212124d
Ярослав Ярошевский <pomidorkaniga228@gmail.com>
2026-09-23 14:28:03 +0300

    added taco design

---
+++ src/main/java/org/dewqryp/tacocloud/data/Taco.java
@@ -0,0 +1,13 @@
+package org.dewqryp.tacocloud.data;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
