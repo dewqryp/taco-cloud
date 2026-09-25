09400ef3c1824c8b4a208e177aa610304f4dc956
Ярослав Ярошевский <pomidorkaniga228@gmail.com>
2026-09-25 12:46:52 +0300

    added more functionallity to securityFilterChain, added oauth2

--- src/main/java/org/dewqryp/tacocloud/security/SecurityConfig.java
+++ src/main/java/org/dewqryp/tacocloud/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                 .requestMatchers("/design", "/orders").hasRole("USER")
                 .requestMatchers("/", "/**").permitAll())
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/design").permitAll())
                 .build();
     }
 }
--- src/test/java/org/dewqryp/tacocloud/HomeControllerTest.java
+++ src/test/java/org/dewqryp/tacocloud/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package org.dewqryp.tacocloud;
 
-import org.dewqryp.tacocloud.controllers.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
